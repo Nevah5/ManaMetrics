eb6bc730e39259f76bf8ea77f6f2c517601d6548
Nevah5 <contact@nevah5.dev>
2025-03-19 18:57:36 +0100

    Added vuetify to project

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,23 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-app.use(router)
+// Components
+import App from './App.vue'
 
-app.mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App)
+.use(vuetify)
+.use(router)
+.mount('#app')
