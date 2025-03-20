c93e1785ddb8b3598e47049ebe6c9b5fcad6ceed
Nevah5 <contact@nevah5.dev>
2025-03-20 09:40:31 +0100

    Set system theme as default color theme

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,30 +17,25 @@
       <v-divider></v-divider>
 
       <v-list density="compact" nav>
-        <v-list-item
-        prepend-icon="mdi-account-group"
-        title="Players"
-        value="players"
-        ></v-list-item>
-        <v-list-item
-        prepend-icon="mdi-controller"
-        title="Games"
-        value="games"
-        ></v-list-item>
+        <v-list-item prepend-icon="mdi-account-group" title="Players" value="players"></v-list-item>
+        <v-list-item prepend-icon="mdi-controller" title="Games" value="games"></v-list-item>
         <v-list-item prepend-icon="mdi-cards" title="Decks" value="decks"></v-list-item>
         <v-list-item prepend-icon="mdi-star" title="Favorites" value="starred"></v-list-item>
       </v-list>
 
       <v-list density="compact" class="fixed-bottom">
         <v-list-item
-          :prepend-icon="
-            theme === 'light' ? 'mdi-white-balance-sunny' : 'mdi-moon-waning-crescent'
-          "
+          :prepend-icon="theme === 'light' ? 'mdi-white-balance-sunny' : 'mdi-moon-waning-crescent'"
           title="Toggle Theme"
           @click="toggleTheme"
         ></v-list-item>
 
-        <v-list-item prepend-icon="mdi-logout" class="text-red-darken-2" title="Logout" @click="logout"></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-logout"
+          class="text-red-darken-2"
+          title="Logout"
+          @click="logout"
+        ></v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-main>
@@ -54,7 +49,7 @@ import { shallowRef } from 'vue'
 import { RouterView } from 'vue-router'
 
 const drawer = shallowRef(false)
-const theme = shallowRef('light')
+const theme = shallowRef(window.matchMedia('(prefers-color-scheme: dark)') ? 'dark' : 'light')
 
 function toggleTheme() {
   theme.value = theme.value === 'light' ? 'dark' : 'light'
