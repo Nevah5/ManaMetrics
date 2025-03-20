db819f4be6691a1594188ae3ec0dae2a1854ccba
Nevah5 <contact@nevah5.dev>
2025-03-20 10:18:32 +0100

    Defined nav items as list

--- frontend/src/components/TopBar.vue
+++ frontend/src/components/TopBar.vue
@@ -1,10 +1,22 @@
 <template>
   <v-app-bar app>
     <v-toolbar-title>MTG Stats</v-toolbar-title>
+
     <v-spacer></v-spacer>
-    <v-app-bar-nav-icon @click="drawer = !drawer" />
+    <v-btn
+      v-for="item in navItems"
+      :key="item.value"
+      :prepend-icon="item.icon"
+      class="d-md-flex d-none"
+      text
+    >
+      {{ item.title }}
+    </v-btn>
+
+    <v-app-bar-nav-icon @click="drawer = !drawer" class="d-md-none" />
   </v-app-bar>
-  <v-navigation-drawer v-model="drawer" location="right" temporary>
+
+  <v-navigation-drawer v-model="drawer" location="right" temporary class="d-md-none">
     <v-list>
       <v-list-item
         prepend-avatar="https://xsgames.co/randomusers/assets/avatars/male/3.jpg"
@@ -16,15 +28,20 @@
     <v-divider></v-divider>
 
     <v-list density="compact" nav>
-      <v-list-item prepend-icon="mdi-account-group" title="Players" value="players"></v-list-item>
-      <v-list-item prepend-icon="mdi-controller" title="Games" value="games"></v-list-item>
-      <v-list-item prepend-icon="mdi-cards" title="Decks" value="decks"></v-list-item>
-      <v-list-item prepend-icon="mdi-star" title="Favorites" value="starred"></v-list-item>
+      <v-list-item
+        v-for="item in navItems"
+        :key="item.value"
+        :prepend-icon="item.icon"
+        :title="item.title"
+        :value="item.value"
+      ></v-list-item>
     </v-list>
 
     <v-list density="compact" class="fixed-bottom">
       <v-list-item
-        :prepend-icon="props.theme === 'light' ? 'mdi-white-balance-sunny' : 'mdi-moon-waning-crescent'"
+        :prepend-icon="
+          props.theme === 'light' ? 'mdi-white-balance-sunny' : 'mdi-moon-waning-crescent'
+        "
         title="Toggle Theme"
         @click="$emit('toggle-theme')"
       ></v-list-item>
@@ -40,15 +57,21 @@
 </template>
 
 <script setup>
-import { shallowRef } from 'vue'
-import { defineProps } from 'vue'
+import { shallowRef, defineProps } from 'vue'
 
 const props = defineProps({
-  theme: { type: String, required: true }
+  theme: { type: String, required: true },
 })
 
 const drawer = shallowRef(false)
-const theme = shallowRef(window.matchMedia('(prefers-color-scheme: dark)') ? 'dark' : 'light')
+
+const navItems = [
+  { icon: 'mdi-account-group', title: 'Players', value: 'players' },
+  { icon: 'mdi-controller', title: 'Games', value: 'games' },
+  { icon: 'mdi-cards', title: 'Decks', value: 'decks' },
+  { icon: 'mdi-star', title: 'Favorites', value: 'starred' },
+  { icon: 'mdi-magnify', title: 'Search', value: 'search' },
+]
 
 function logout() {
   console.log('logout')
