794af6fe7fce43627b1e475c6f6f365ef5d7fcf7
Nevah5 <contact@nevah5.dev>
2025-03-28 23:42:05 +0100

    Added search bar to navbar

--- frontend/src/components/TopBar.vue
+++ frontend/src/components/TopBar.vue
@@ -5,28 +5,63 @@
 
       <div class="d-flex align-center ga-3">
         <nav-button :items="items" />
-        <v-divider class="mx-3" vertical></v-divider>
-        <nav-button :items="personal" />
+        <v-divider class="mx-3" vertical v-if="isLoggedIn"></v-divider>
+        <nav-button :items="personal" v-if="isLoggedIn" />
       </div>
     </div>
 
     <v-spacer></v-spacer>
 
-    <v-btn
-      :icon="theme === 'dark' ? 'mdi-white-balance-sunny' : 'mdi-weather-night'"
-      :color="theme === 'dark' ? 'white' : 'black'"
-      class="ma-2"
-      @click="$emit('toggleTheme')"
-    ></v-btn>
+    <div class="d-flex align-center ga-3">
+      <v-text-field
+        v-model="search"
+        density="compact"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        variant="solo-filled"
+        flat
+        hide-details
+        single-line
+        class="search-bar mr-5"
+      ></v-text-field>
+      <router-link class="reset-style" to="/login">Login</router-link>
+      <router-link class="reset-style" to="/register">Register</router-link>
+      <v-btn
+        :icon="theme === 'dark' ? 'mdi-white-balance-sunny' : 'mdi-weather-night'"
+        :color="theme === 'dark' ? 'white' : 'black'"
+        class="ma-2"
+        @click="$emit('toggleTheme')"
+      ></v-btn>
+    </div>
   </v-app-bar>
 </template>
 
 <script setup>
 import NavButton from './buttons/NavButton.vue';
 import { items, personal } from '@/configs/navbar';
-import { defineProps } from 'vue'
+import { defineProps, onMounted, shallowRef } from 'vue'
 
+const isLoggedIn = shallowRef(false);
 const props = defineProps({
   theme: { type: String, required: true },
 })
+
+onMounted(() => {
+  const isLogin = localStorage.getItem('is_login');
+  if (isLogin) {
+    isLoggedIn.value = true;
+  } else {
+    isLoggedIn.value = false;
+  }
+});
 </script>
+
+<style lang="scss" scoped>
+a.reset-style {
+  color: inherit;
+  text-decoration: none;
+}
+.search-bar {
+  width: 200px;
+}
+</style>
