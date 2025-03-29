400594fefcfdfad836555897f54b8186e089ba11
Nevah5 <contact@nevah5.dev>
2025-03-29 15:14:46 +0100

    Hide some parts in navbar on smaller screens and added temp popover for login

--- frontend/src/components/TopBar.vue
+++ frontend/src/components/TopBar.vue
@@ -3,7 +3,7 @@
     <div class="d-flex align-center ga-10 ml-5">
       <v-toolbar-title>MTG Stats</v-toolbar-title>
 
-      <div class="d-flex align-center ga-3">
+      <div class="align-center ga-3 d-none d-md-flex">
         <nav-button :items="items" />
         <v-divider class="mx-3" vertical v-if="isLoggedIn"></v-divider>
         <nav-button :items="personal" v-if="isLoggedIn" />
@@ -22,16 +22,87 @@
         flat
         hide-details
         single-line
-        class="search-bar mr-5"
+        class="search-bar mr-5 d-none d-md-block"
       ></v-text-field>
-      <router-link class="reset-style" to="/login">Login</router-link>
-      <router-link class="reset-style" to="/register">Register</router-link>
+      <router-link class="reset-style d-none d-md-flex" to="/login">Login</router-link>
+      <router-link class="reset-style d-none d-md-flex" to="/register">Register</router-link>
       <v-btn
         :icon="theme === 'dark' ? 'mdi-white-balance-sunny' : 'mdi-weather-night'"
         :color="theme === 'dark' ? 'white' : 'black'"
-        class="ma-2"
+        class="ma-2 d-none d-md-flex"
         @click="$emit('toggleTheme')"
       ></v-btn>
+
+      <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      location="end"
+    >
+    <template v-slot:activator="{ props }">
+      <v-avatar color="info" class="ma-2 d-flex d-md-none"v-bind="props">
+          <v-icon icon="mdi-account-circle"></v-icon>
+        </v-avatar>
+      </template>
+
+      <v-card min-width="300">
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://cdn.vuetifyjs.com/images/john.jpg"
+            subtitle="Founder of Vuetify"
+            title="John Leider"
+          >
+            <template v-slot:append>
+              <v-btn
+                :class="fav ? 'text-red' : ''"
+                icon="mdi-heart"
+                variant="text"
+                @click="fav = !fav"
+              ></v-btn>
+            </template>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list>
+          <v-list-item>
+            <v-switch
+              v-model="message"
+              color="purple"
+              label="Enable messages"
+              hide-details
+            ></v-switch>
+          </v-list-item>
+
+          <v-list-item>
+            <v-switch
+              v-model="hints"
+              color="purple"
+              label="Enable hints"
+              hide-details
+            ></v-switch>
+          </v-list-item>
+        </v-list>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            variant="text"
+            @click="menu = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            color="primary"
+            variant="text"
+            @click="menu = false"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-menu>
     </div>
   </v-app-bar>
 </template>
@@ -39,12 +110,17 @@
 <script setup>
 import NavButton from './buttons/NavButton.vue';
 import { items, personal } from '@/configs/navbar';
-import { defineProps, onMounted, shallowRef } from 'vue'
+import { defineProps, onMounted, shallowRef, ref } from 'vue'
 
 const isLoggedIn = shallowRef(false);
 const props = defineProps({
   theme: { type: String, required: true },
 })
+
+const fav = ref(true)
+const menu = ref(false)
+const message = ref(false)
+const hints = ref(true)
 
 onMounted(() => {
   const isLogin = localStorage.getItem('is_login');
@@ -63,5 +139,10 @@ a.reset-style {
 }
 .search-bar {
   width: 200px;
+}
+.speed-dial {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
 }
 </style>
