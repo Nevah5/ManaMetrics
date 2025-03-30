3b3c6e10811f9c087bbb627a3c33d82abbb03b80
Nevah5 <contact@nevah5.dev>
2025-03-30 18:21:14 +0200

    Added example last games section to dashboard

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import FeaturedStats from '@/components/dashboard/FeaturedStats.vue';
+import LastGames from '@/components/dashboard/LastGames.vue';
 import TopDecks from '@/components/dashboard/TopDecks.vue';
 import TopPlayers from '@/components/dashboard/TopPlayers.vue';
 
@@ -8,6 +9,7 @@ import TopPlayers from '@/components/dashboard/TopPlayers.vue';
 <template>
   <h1>Dashboard</h1>
   <FeaturedStats />
+  <LastGames />
   <TopPlayers />
   <TopDecks />
 </template>
