0ec65437d3364e81d29239867298a9c0e0675208
Nevah5 <contact@nevah5.dev>
2025-03-30 19:50:32 +0200

    Updated featured stats

--- frontend/src/components/dashboard/FeaturedStats.vue
+++ frontend/src/components/dashboard/FeaturedStats.vue
@@ -7,21 +7,21 @@
     <div class="d-flex flex-wrap ga-4 justify-center">
       <v-card class="pb-4">
         <v-card-title>
-          Total Players
+          Your Wins
         </v-card-title>
         <v-card-subtitle class="text-h4">1,234</v-card-subtitle>
       </v-card>
       <v-card class="pb-4">
         <v-card-title>
           <v-icon icon="mdi-gamepad-variant-outline" size="small" />
-          Total Games
+          Your Games
         </v-card-title>
         <v-card-subtitle class="text-h4">4,321</v-card-subtitle>
       </v-card>
       <v-card class="pb-4">
         <v-card-title>
           <v-icon icon="mdi-gamepad-variant-outline" size="small" />
-          Total Games
+          Your Games
           <v-chip size="small" color="blue" append-icon="mdi-clock-time-eight-outline">last 30 days</v-chip>
         </v-card-title>
         <v-card-subtitle class="text-h4">1,234</v-card-subtitle>
@@ -29,14 +29,14 @@
       <v-card class="pb-4" href="/decks/featured">
         <v-card-title>
           <v-icon icon="mdi-cards-outline" size="small" />
-          Top Deck
+          Your Top Deck
         </v-card-title>
         <v-card-subtitle class="text-h4">Koma Serpent V2</v-card-subtitle>
       </v-card>
       <v-card class="pb-4" href="decks/newest">
         <v-card-title>
           <v-icon icon="mdi-cards-outline" size="small" />
-          Newest Deck
+          Your Newest Deck
         </v-card-title>
         <v-card-subtitle class="text-h4">Koma Serpent V2</v-card-subtitle>
       </v-card>
