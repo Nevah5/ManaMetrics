9e7d4547c1ec22f49c3fe20c58cb977544e83057
Nevah5 <contact@nevah5.dev>
2025-04-20 00:00:39 +0200

    Changed database schema file

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,17 +1,37 @@
-DROP TABLE IF EXISTS participants;
+DROP TABLE IF EXISTS group_members;
+DROP TABLE IF EXISTS game_participants;
+DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS games;
 DROP TABLE IF EXISTS decks;
-DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS groups;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE IF NOT EXISTS users (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  display_name VARCHAR(31) NOT NULL,
+  display_name VARCHAR(32) NOT NULL,
   username VARCHAR(24) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password_hash VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS groups (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(32) NOT NULL,
+  owner_id INT NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (owner_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS group_members (
+  group_id INT NOT NULL,
+  user_id INT NOT NULL,
+  create_games BOOLEAN NOT NULL DEFAULT FALSE,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (group_id, user_id),
+  FOREIGN KEY (group_id) REFERENCES groups(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS roles (
@@ -24,29 +44,33 @@ CREATE TABLE IF NOT EXISTS user_roles (
   user_id INT NOT NULL,
   role_id INT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (user_id, role_id),
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
 CREATE TABLE IF NOT EXISTS decks (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  user_id INT NOT NULL,
+  creator_id INT NOT NULL,
   name VARCHAR(255) NOT NULL,
   link VARCHAR(255) NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  added_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (creator_id) REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS games (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  group_owner INT NOT NULL,
+  created_by INT NOT NULL,
   location VARCHAR(255) NOT NULL,
-  winner_id INT NOT NULL,
   notes TEXT,
   played_at DATETIME NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (winner_id) REFERENCES users(id)
+  FOREIGN KEY (group_owner) REFERENCES groups(id),
+  FOREIGN KEY (created_by) REFERENCES users(id)
 );
 
-CREATE TABLE IF NOT EXISTS participants (
+CREATE TABLE IF NOT EXISTS game_participants (
   user_id INT,
   deck_id INT,
   game_id INT NOT NULL,
@@ -55,5 +79,6 @@ CREATE TABLE IF NOT EXISTS participants (
   comment VARCHAR(255),
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (game_id) REFERENCES games(id),
-  FOREIGN KEY (deck_id) REFERENCES decks(id)
+  FOREIGN KEY (deck_id) REFERENCES decks(id),
+  PRIMARY KEY (game_id, user_id, guest_name)
 );
